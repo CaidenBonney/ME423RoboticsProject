af4ef0b2aff4acffda4bb52cc9dfcc10e518e583
AntonioVentimiglia <ventimaj@icloud.com>
2026-02-16 14:01:35 -0800

    Ball detect code updated

--- src/BallDetect.py
+++ src/BallDetect.py
@@ -13,8 +13,8 @@ import numpy as np
 # print(os.listdir())
  
 # Read image
-im = cv2.imread("pictures/ball_1.jpg", cv2.IMREAD_GRAYSCALE)
- 
+im = cv2.imread("pictures/ball_3.jpg", cv2.IMREAD_GRAYSCALE)
+
 # Set up the detector with default parameters.
 params = cv2.SimpleBlobDetector_Params()
 
@@ -24,23 +24,38 @@ params.blobColor = 255
 
 # Size filtering (tune minArea for your image)
 params.filterByArea = True
-params.minArea = 100 # used to be 100
-params.maxArea = 200000
+params.minArea = 1000 # used to be 100
+params.maxArea = 200_000
 
 # Shape filtering (reject edges/corners)
 params.filterByCircularity = True
-params.minCircularity = 0.7 # larger number because ping pong ball are round ash
+params.minCircularity = 0.6 # larger number because ping pong ball are round ash
 
 params.filterByInertia = True # used to be false
-params.minInertiaRatio = 0.5
+params.minInertiaRatio = 0.1
 
 params.filterByConvexity = True # used to be false
-params.minConvexity = 0.8
+params.minConvexity = 0.5
 
 detector = cv2.SimpleBlobDetector_create(params)
  
+blur = cv2.GaussianBlur(im, (9,9), 0)
+
+# adaptive threshold handles uneven lighting / shadows
+bw = cv2.adaptiveThreshold(
+    blur, 255,
+    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    cv2.THRESH_BINARY,
+    101,  # blockSize (odd) -> bigger = smoother lighting model
+    -15   # C -> shifts threshold; make more negative to include darker parts
+)
+
+# fill in shadow gaps / holes
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+bw = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel, iterations=1)
+
 # Detect blobs.
-keypoints = detector.detect(im)
+keypoints = detector.detect(bw)
  
 # Draw detected blobs as red circles.
 # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
@@ -48,4 +63,5 @@ im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv
  
 # Show keypoints
 cv2.imshow("Keypoints", im_with_keypoints)
+cv2.imshow("bw", bw)
 cv2.waitKey(0)
