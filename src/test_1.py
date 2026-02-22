9501bfef7bc5554ca89f8d9eec92436af0507e03
MichaelHabib04 <129786412+MichaelHabib04@users.noreply.github.com>
2026-02-22 12:03:10 -0800

    test script is now times

--- src/test_1.py
+++ src/test_1.py
@@ -1,6 +1,7 @@
 # Standard imports
 import cv2
 import numpy as np
+import time
 # import os
 # print(os.listdir())
  
@@ -17,12 +18,15 @@ params.filterByConvexity = False
 params.filterByInertia = False
 detector = cv2.SimpleBlobDetector_create(params)
  
+start = time.time()
 # Detect blobs.
 keypoints = detector.detect(im)
- 
+end = time.time() 
+print(f'detection time: {end - start}')
 # Draw detected blobs as red circles.
 # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
 im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv2.imwrite("detected.png", im_with_keypoints)
  
 # Show keypoints
 cv2.imshow("Keypoints", im_with_keypoints)
